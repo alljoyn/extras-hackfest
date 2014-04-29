03d4ef57c8eb191a8473eb75f58184b4d9325d71
Wayne Lee <waynelee@qce.qualcomm.com>
2014-04-29 03:02:49 -0700

    add linino myapp package and source
    Change-Id: Ic29c733419087820d285607497f5a8a2c3b28dea
    Signed-off-by: Wayne Lee <waynelee@qce.qualcomm.com>

---
+++ aj_samples/myapp/src/solutions/hello_print_string.cc
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Copyright (c) 2014, AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ ******************************************************************************/
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <aj_tutorial/display.h>
+
+#define msleep(x) usleep((x) * 1000)
+
+int display_str(char* str) {
+    Display display;
+
+    while (*str != 0) {
+        display.DrawCharacter(*str);
+        msleep(300);
+        str++;
+    }
+    display.DrawCharacter(' ');
+}
+  
+int main(int argc, char**argv, char**envArg)
+{
+    if (argc >= 2) {
+      display_str(argv[1]);
+    } else {
+      display_str("rocks dude");
+    }
+    return 1;
+} 
